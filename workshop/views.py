e3a665fcfdaa665224697a87f6d784c02d277e28
Pari9D <parahem.asfaw@aastustudent.edu.et>
2026-04-17 17:56:13 +0300

    update

--- workshop/views.py
+++ workshop/views.py
@@ -4,3 +4,14 @@ from .models import Bagan
 def home(request):
     bagans = Bagan.objects.all()  # Fetch all Begenas
     return render(request, 'home.html', {'bagans': bagans})
+
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from .models import Bagan
+from .serializers import BaganSerializer
+
+@api_view(['GET'])
+def bagan_list_api(request):
+    bagans = Bagan.objects.all()
+    serializer = BaganSerializer(bagans, many=True)
+    return Response(serializer.data)
